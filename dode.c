7ee04baa64c5cff0abc0898bbaaefe8a549d7c5b
wooden-chair <alraybaig@gmail.com>
2026-07-29 12:03:25 +0300

    just a simple C script
    Will not be used and should not be used in further writing in any way. It's just a "hello world" script for nothing, no purpose.

---
+++ dode.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(){
+    char cement[] = "amongus";
+    printf("your name is... %s\n", cement);
+
+    bool mangoRaw = true;   
+
+    if (mangoRaw){
+        printf("mango might be raw, yes.\n");
+    } else {
+        printf("mango is smooth, yes\n");
+    }
+
+    return 0;
+}
